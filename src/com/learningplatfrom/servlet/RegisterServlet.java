fe365284531545e8998e6a12e868a75188292128
yangxiaowan <632024528@qq.com>
2015-11-21 20:25:34 +0800

    2015/11/21/04

--- src/com/learningplatfrom/servlet/RegisterServlet.java
+++ src/com/learningplatfrom/servlet/RegisterServlet.java
@@ -17,10 +17,10 @@ public class RegisterServlet extends HttpServlet {
 	}
 	public void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+		doPost(request,response);
 	}
 	public void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-			
 	}
 	public void init() throws ServletException {
 		
