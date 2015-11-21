d3ec2b64e931f8bea40741601237a3bfabf9f38b
yangxiaowan <632024528@qq.com>
2015-11-21 17:23:35 +0800

    2015/11/211/2

---
+++ src/com/learningplatfrom/servlet/RegisterServlet.java
@@ -0,0 +1,28 @@
+package com.learningplatfrom.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class RegisterServlet extends HttpServlet {
+	public RegisterServlet() {
+		super();
+	}
+	public void destroy() {
+		super.destroy();  
+	}
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+	}
+
+	public void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+	}
+	public void init() throws ServletException {
+	}
+}
